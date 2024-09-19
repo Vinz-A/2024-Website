02a721087f630b3d70c22d6d6aa74d9e840ca1cf
Vinz-A <vinz_abu@student.asdah.school.nz>
2024-09-20 11:35:55 +1200

    Added a Java script + changes/fixes

--- evidence.js
+++ evidence.js
@@ -1,7 +1,36 @@
 let slideIndex = 0;
 
+//slide show
 function showslides() {
-    const slides = document.queryselctorAll('.carousel-slide img');
+    const slides = document.querySelectorAll('.carousel-slide img');
     slides.forEach((slide, index) => {
         slide.style.display = "none"; });
+     //Hide all images
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1; //Reset to first Imaage
     }
+
+    slides[slideIndex - 1].style.display = "block"; //Display current Image
+    setTimeout(showslides, 2000); // Changes Image every 2 seconds
+
+    }
+
+    //Buttons
+    function moveSlides(n) {
+        slideIndex += n; 
+        const slides = document.querySelectorAll('.carousel-slide img');
+        if (slideIndex < 1) {
+            slideIndex = slides.length;
+        }
+        else if (slideIndex > slides.length){
+            slideIndex = 1;
+        }
+        slides.forEach((slide, index) =>{
+            slide.style.display = "none"; //Hides all images
+        });
+        slides[slideIndex - 1].style.display = "block"; //Display the current image
+    }
+
+    showslides(); //Initilaze the slideshow
